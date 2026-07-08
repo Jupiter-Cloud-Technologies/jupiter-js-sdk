299869a7c0f4ee9f2c1f5b92ba773ea4fcf7bb31
Dominik7787 <poulosdominik29@gmail.com>
2026-07-09 00:20:02 +0200

    feat: UUID validation in admin

--- packages/auth/src/internal/validation.ts
+++ packages/auth/src/internal/validation.ts
@@ -5,3 +5,9 @@ export function validateProjectId(str: string) {
     throw new Error('Invalid Project ID. Project Id must be a valid UUID')
   }
 }
+
+export function validateUUID(str: string) {
+  if (!UUID_REGEX.test(str)) {
+    throw new Error('Invalid Project ID. Project Id must be a valid UUID')
+  }
+}
