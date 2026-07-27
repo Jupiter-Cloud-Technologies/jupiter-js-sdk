8f6f231512314c27d364cd20254ee3156e2c8c1c
Dominik7787 <poulosdominik29@gmail.com>
2026-07-27 08:30:31 +0200

    feat: enforce storage method params

--- packages/storage/test/types/index.test-d.ts
+++ packages/storage/test/types/index.test-d.ts
@@ -5,10 +5,19 @@ const storage = new JupiterStorage('https://storage.example.test', {
   projectId: 'project-1'
 })
 
-expectError(storage.uploadObject('forms', 'multipart', new FormData()))
 expectError(
-  storage.uploadMultipartPart('forms', 'upload-1', new FormData(), {
+  storage.uploadObject({
+    body: new FormData(),
+    bucketName: 'forms',
+    key: 'multipart'
+  })
+)
+expectError(
+  storage.uploadMultipartPart({
+    body: new FormData(),
+    bucketName: 'forms',
     contentLength: 1,
-    partNumber: 1
+    partNumber: 1,
+    uploadId: 'upload-1'
   })
 )
